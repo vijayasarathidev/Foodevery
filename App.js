eca9911247b69c87727fbb368c1b0700caf42015
vijayasarathi <113992612+vijayasarathidev@users.noreply.github.com>
2023-07-11 15:29:07 +0530

    commit

--- App.js
+++ App.js
@@ -3,14 +3,24 @@
  * ReactDOM.createRoot only creates the html element
  * we can create teo child in a parent by passing them as an array
  */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div",
-    { id: "parent" },
-    [React.createElement("h1", {}, "Hello child 1"),React.createElement("h2", {}, "Hello child 2"),React.createElement("h3", {}, "Hello child 3")]
-  );
-  
-  const root = ReactDOM.createRoot(document.getElementById("root"));
-  
-  root.render(parent);
-  
+
+const HeadingComponent = () => (
+  <div id="container">
+    <h1 className="heading"> Hello Akshay saini</h1>
+  </div>
+);
+
+const title = (
+  <h1>
+    Hello giri
+    <HeadingComponent/>
+  </h1>
+);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(title);
